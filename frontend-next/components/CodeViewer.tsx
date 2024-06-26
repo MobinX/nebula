1bcc950d718a23e517ac9376a09b9b05a6c2343f
MobinX <mobin0219@gmail.com>
2024-06-26 10:47:22 +0100

    done

--- frontend-next/components/CodeViewer.tsx
+++ frontend-next/components/CodeViewer.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { FlipContext } from './CardFlipper';
-import { Code2 } from 'lucide-react';
+import { Code2, View } from 'lucide-react';
 
 export default function CodeViewer({ code }: { code: string }) {
     const { toggleFlip } = useContext(FlipContext);
@@ -11,7 +11,7 @@ export default function CodeViewer({ code }: { code: string }) {
         <div className="mockup-code  border-base-300 border w-full h-full rounded-3xl">
             <div className="flex items-center gap-2 absolute top-[15px] right-[7px]">
                 <div onClick={() => toggleFlip()} className="">
-                    <Code2 className="w-6 h-6 " />
+                <View  className="w-6 h-6 " />
                 </div>
             </div>
             <SyntaxHighlighter customStyle={{ background: "transparent", boxShadow: "unset", height: "100%", border: 0 }} language="javascript" style={dark}>
