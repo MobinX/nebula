f346efd8788751a09ba3d62d1d3f58b092af5dc4
MobinX <mobin0219@gmail.com>
2024-07-24 14:31:50 +0600

    alhamdulillah

--- frontend-next/app/(file_manager)/components/ActionBar.tsx
+++ frontend-next/app/(file_manager)/components/ActionBar.tsx
@@ -11,13 +11,13 @@ export const ActionBar = () => {
     const prams = useSearchParams()
     const location = prams.get("location")
     return (
-        <div className="flex w-full gap-2 items-center justify-center my-3 px-2 py-4 bg-base-content/30 rounded-2xl" >
+        <div className="flex w-full gap-2 items-center justify-center my-3 px-2 py-2 bg-base-content/30 rounded-2xl" >
             <button className="btn btn-circle btn-sm btn-ghost" onClick={async () => await goBack()}><ArrowLeft className="w-5 h-5" /></button>
             <div className="breadcrumbs overflow-x-visible flex-1 w-full flex items-center bg-base-content/50 px-2 py-1 rounded-2xl ">
                 <ul>
                     {(location?.split("/"))?.map((path,key)=>(
                         <li key={key}>
-                            <Link href={"/fileManager?location=" + path } onClick={()=> setIncludeInHistory(true)}>{path}</Link>
+                            <Link href={"/fileManager?location=" + location?.split("/").slice(0, key+1).join("/") } onClick={()=> setIncludeInHistory(true)}>{path}</Link>
                         </li>
                     ))}
                 </ul>
